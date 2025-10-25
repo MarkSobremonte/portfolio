b8896631f3cb86a0ae817b3198a2f51deb2e8792
MarkSobremonte <marksobremonte1020@gmail.com>
2025-10-25 19:30:57 +0800

    3rd commit

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,11 +1,19 @@
+'use client'
 import React from 'react'
-
-const hero = () => {
+import Image from 'next/image'
+const Hero = () => {
   return (
-    <div>
-      
+    <div className='relative h-screen flex items-center justify-center text-black overflow-hidden flex-col'>
+      <div className='relative z-10 flex flex-col items-center'>
+        <Image 
+        src="/images/s1.jpg" 
+        alt="heroimage" 
+        width={200}
+        height={150}
+        className="rounded-full border-5 border-[#000000]"/> 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default hero
+export default Hero
